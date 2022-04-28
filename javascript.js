108b29145374e96eb44c4c10fe6c478ea554552e
arsyahmad12 <arsyahmad14@gmail.com>
2022-04-28 16:02:49 +0700

    Brought Back The Functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
+function add(a, b) {
+  return a+b;
+}
 
+function subtract(a, b) {
+  return a-b;
+}
+
+function multiply(a, b) {
+  return a*b;
+}
+
+function divide(a, b) {
+  return a/b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      add(a, b);
+    case "-":
+      subtract(a, b);
+    case "*":
+      multiply(a, b);
+    case "/":
+      divide(a, b);
+    default:
+      console.log("Please put the right Mathematical Operator")
+  }
+}
